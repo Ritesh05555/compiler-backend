6b5bb6f611454618635a4fe5b3a1e5dbd8ecc535
Ritesh05555 <shubamsahani03@gmail.com>
2025-06-19 17:19:33 +0530

    time extended for code execution for python and java

--- server.js
+++ server.js
@@ -1813,7 +1813,7 @@ const execPromise = util.promisify(exec);
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:'http://localhost:5173',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 }));
